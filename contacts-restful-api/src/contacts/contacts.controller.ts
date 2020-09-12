0015276ddfc4b064bdcca23c3f61084941cb5b68
jasonj85 <jasonj85@gmail.com>
2020-09-12 10:30:31 +0100

    Initial creation of restful api using NextJs

---
+++ contacts-restful-api/src/contacts/contacts.controller.ts
@@ -0,0 +1,73 @@
+import { Body, Controller, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+
+@Controller('contacts')
+export class ContactsController {
+    contacts = [
+        {
+            id: 1, name: 'Jason', email: 'jason@email.com'
+        },
+        {
+            id: 2, name: 'Tim', email: 'Tim@email.com'
+        },
+        {
+            id: 3, name: 'Bob', email: 'Bob@email.com'
+        },
+        {
+            id: 4, name: 'June', email: 'June@email.com'
+        },
+    ];
+
+    @Get()
+    getAll() {
+        return [...this.contacts];
+    }
+
+    @Get('/:contactId')
+    getOne(@Param('contactId') id: number) {
+        let contact = this.contacts.find(c => c.id == id);
+
+        if(!contact) {
+            throw new NotFoundException();
+        }
+
+        return {...contact};
+    }
+
+    @Post()
+    createContacts(@Body() body) {
+        let ids = this.contacts.map(c => c.id);
+        let newId = Math.max(...ids) + 1;
+        let out = null;
+
+        if (body instanceof Array) {
+            let newContacts = body;
+            newContacts.forEach((c, i) => c.id = newId + 1);
+
+            this.contacts.push(...newContacts)
+            out = newContacts;
+        } else {
+            let newContact = body
+            newContact.id = newId;
+
+            this.contacts.push(newContact);
+            out = newContact;
+        }
+
+        return out;
+    }
+
+    @Put('/:id')
+    updateContact(@Param('id') id, @Body() updatedContact) {    
+        let index = this.contacts.findIndex(c => c.id == id);
+        
+        if(index === -1) {
+            throw new NotFoundException();
+        }
+
+        updatedContact.id = parseInt(id);
+        this.contacts[index] = updatedContact;
+
+        return updatedContact;
+    }
+
+}
