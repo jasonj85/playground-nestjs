aa7e121ff1ddf524073d74510463dc9e1cf4beda
jasonj85 <jasonj85@gmail.com>
2020-09-17 08:35:19 +0100

    Renamed to nest-basic-demo

---
+++ nest-basic-demo/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ContactsModule } from './contacts/contacts.module';
+
+@Module({
+  imports: [ContactsModule],
+  controllers: [AppController],
+})
+export class AppModule {}
