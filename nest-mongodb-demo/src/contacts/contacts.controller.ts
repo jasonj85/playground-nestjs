e78b73db5f55e5e98797d9287603c13f43134188
jasonj85 <jasonj85@gmail.com>
2020-09-19 09:40:19 +0100

    Updates to contacts service and controller

--- nest-mongodb-demo/src/contacts/contacts.controller.ts
+++ nest-mongodb-demo/src/contacts/contacts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, DefaultValuePipe, Get, ParseIntPipe, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Body, Controller, DefaultValuePipe, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, ParseIntPipe, Patch, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Contact } from './contact.schema';
 import { ContactsService } from './contacts.service';
 
@@ -8,14 +8,62 @@ export class ContactsController {
 
     @Post()
     @UsePipes(new ValidationPipe({ transform: true }))
-    createContact(@Body() body: Contact) {
-        return this.service.addContact(body);
+    async createContact(@Body() body: Contact) {
+        try {
+            return await this.service.addContact(body);
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
     }
 
     @Get()
-    getAll(@Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number, @Query('limit', new DefaultValuePipe(100), ParseIntPipe) limit: number) {
-        return this.service.getContacts(page, limit);
+    async getAll(@Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number, @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number) {
+        try {
+           return await this.service.getContacts(page, limit);            
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
     }
 
+    @Get('/:id')
+    async getContact(@Param('id') id: string) {
+        try {
+            return await this.service.getContact(id);     
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+    }
+    
+    @Put('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async updateFullContact(@Param('id') id: string, @Body() body: Contact) {
+        try {
+            await this.service.updateFullContact(id, body);
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+    }
 
+    @Patch('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async updatePartialContact(@Param('id') id: string, @Body() props) {
+        try {
+            await this.service.updatePartialContact(id, props);
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async deleteContact(@Param('id') id: string) {
+        try {
+            let res = await this.service.deleteContact(id);
+            if (res.deletedCount === 0) throw new NotFoundException();
+        } catch (e) {
+            throw new BadRequestException(e);
+        }
+    }
 }
--- nest-mongodb-demo/src/contacts/contacts.service.ts
+++ nest-mongodb-demo/src/contacts/contacts.service.ts
@@ -5,29 +5,32 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ContactsService {
-
     constructor(@InjectModel('Contact') private ContactModel: Model<Contact>) { }
 
     addContact(contact: Contact) {
 
         let c = new this.ContactModel({ ...contact });
-        return c.save()
-            .then(result => {
-                return result;
-            })
-            .catch(err => {
-                return err;
-            });
+        return c.save();
     }
 
     getContacts(page: number, limit: number) {
-       return this.ContactModel.find().limit(limit).skip((page-1)*limit)
-            .then(result => {
-                return result;
-            })
-            .catch(err => {
-                return err;
-            });
+        return this.ContactModel.find().limit(limit).skip((page - 1) * limit);
+    }
+
+    getContact(id: string) {
+        return this.ContactModel.findById(id);
+    }
+
+    updateFullContact(id: string, contact: Contact) {
+        return this.ContactModel.updateOne({ _id: id }, contact);
+    }
+
+    updatePartialContact(id: string, props: any) {
+        return this.ContactModel.updateOne({ _id: id }, { $set: props });
+    }
+
+    deleteContact(id: string) {
+        return this.ContactModel.deleteOne({ _id: id });
     }
 
 }
